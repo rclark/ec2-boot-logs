6571132b03926c27aa465d732c35506582293e35
Ryan Clark <ryan.clark.j@gmail.com>
2017-05-04 11:11:31 -0700

    make the cli work

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,9 @@
+#!/usr/bin/env node
+
 'use strict';
 
 const meow = require('meow');
+const getLogs = require('..');
 
 const cli = meow(`
   USAGE: ec2-boot-logs [options]
@@ -8,4 +11,15 @@ const cli = meow(`
   Options:
     -i, --instance-id   (required) the EC2 ID
     -r, --region        [default us-east-1] the EC2 region
-`)
+`, {
+  alias: {
+    i: 'instance-id',
+    r: 'region'
+  },
+  default: {
+    region: 'us-east-1'
+  }
+});
+
+getLogs(cli.flags.instanceId, cli.flags.region, process.stdout)
+  .catch((err) => process.stdout.write(err.stack));
